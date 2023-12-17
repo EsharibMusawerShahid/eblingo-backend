cdcf3e05ac11ff46c1fb145c1bbd18619da31206
Abbas Mehdi <abbasmehdi692@gmail.com>
2023-12-17 22:45:03 +0500

    eblingo backend

---
+++ index.js
@@ -0,0 +1,18 @@
+const dotenv = require('dotenv');
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+dotenv.config({ path: './config.env' });
+require('./db/connect');
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));
+app.use(require('./router/auth'));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
